1360896ecb1d3cb958c1a8fc3e53f96f48c407d8
Harsha Poojari <seeker.goblet5der@gmail.com>
2022-12-08 23:11:53 +0100

    Revert "Donate"

--- src/main.js
+++ src/main.js
@@ -2,8 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { Inkline, components } from '@inkline/inkline';
-import '@inkline/inkline/inkline.scss';
-import "./main.scss";
-   
-createApp(App).use(store).use(router).use(Inkline, {components}).mount('#app')
+
+createApp(App).use(store).use(router).mount('#app')
--- src/views/DonateView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="donate">
-        <div class="_color:primary-05">Primary 5%</div>
-    </div>
-  </template>

54e7bf02a5c20746d395b15531811b60d5b21b23
Shubham Bajaj <77834591+Antimalware-exe@users.noreply.github.com>
2022-12-11 13:04:22 +0100

    Added admin login page in bootstrap: removed inkline (#5)
    * Added login route
    
    * added axios and integrated with login component
    
    * removed inkline, added bootstrap, added login page

---
+++ src/components/Login.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="card loginBox shadow rounded">
+    <h5 class="card-header">Sign in</h5>
+    <div class="card-body">
+      <form @submit="onSubmit">
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Email address</label>
+          <input type="email" class="form-control" required v-model="email" id="exampleInputEmail1"
+            aria-describedby="emailHelp" name="username">
+          <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Password</label>
+          <input type="password" required name="password" v-model="password" class="form-control"
+            id="exampleInputPassword1">
+        </div>
+        <!-- <div class="mb-3 form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1">
+          <label class="form-check-label" for="exampleCheck1">Show password</label>
+        </div> -->
+        <button type="submit" class="btn btn-primary">Login</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import Axios from 'axios'
+export default {
+  name: "Login",
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault();
+      let loginInfoCorrect = false;
+
+      Axios.get(`http://localhost:5002/users?username=${this.email}`)
+        .then(res => loginInfoCorrect = res.data.length > 0 ? (res.data[0].username === this.email && res.data[0].password === this.password) : false
+        )
+        .then(() => {
+          if (loginInfoCorrect) {
+            this.$router.push("admin")
+          } else alert("Incorrect username or password")
+        })
+        .catch(err => { console.log(err.message) })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.loginBox {
+  text-align: start;
+}
+</style>

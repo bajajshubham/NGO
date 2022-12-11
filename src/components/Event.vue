3f2cddb6fce8c52705b073bf01c2a7572d8af7e9
Shubham Bajaj <77834591+Antimalware-exe@users.noreply.github.com>
2022-12-11 22:24:13 +0100

    view events in admin page (#6)
    * Added login route
    
    * added axios and integrated with login component
    
    * removed inkline, added bootstrap, added login page
    
    * display events on admin page

---
+++ src/components/Event.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card" style="width: 18rem;">
+    <img :src="ev.image" class="card-img-top" alt="Img" />
+    <div class="card-body">
+      <h5 class="card-title">{{ ev.title }}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">{{ ev.dateTime }}</h6>
+      <p class="card-text">{{ ev.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Event",
+  props: {
+    ev: Object
+  },
+}
+</script>
--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,3 +1,22 @@
 <template>
-  This is admin page!
+  <div class="p-2 d-flex flex-row admin-body">
+    <!-- <div class="sideNav">Nav</div> -->
+    <div class="admin-body">
+      <Events />
+    </div>
+  </div>
 </template>
+<script>
+import Events from '@/components/Events.vue'
+
+export default {
+  name: "AdminView",
+  components: { Events }
+}
+</script>
+<style scoped>
+.admin-body {
+  /* border: 1px solid blue; */
+  text-align: start;
+}
+</style>

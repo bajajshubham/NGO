e42320a8fa815bce99e4bce0ac8e8c61e110865d
Harsha Poojari <seeker.goblet5der@gmail.com>
2022-12-11 22:00:54 +0100

    Donate form

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <Navbar></Navbar>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
@@ -6,6 +7,17 @@
   </nav>
   <router-view/>
 </template>
+
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+ components: {
+  Navbar
+ }
+}
+</script>
 
 <style lang="scss">
 #app {
--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { Inkline, components } from '@inkline/inkline';
-import '@inkline/inkline/inkline.scss';
-import "./main.scss";
-   
-createApp(App).use(store).use(router).use(Inkline, {components}).mount('#app')
+
+import'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
+
+createApp(App).use(store).use(router).use(bootstrap).mount('#app')

d71293b81c32055d10db6800918d0bd8f948faff
Shubham Bajaj <shubham2bajaj@gmail.com>
2022-12-15 16:53:26 +0100

    implemented vue store feature

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -1,46 +1,31 @@
 <template>
   <h3>Upcoming events</h3>
   <div class="d-flex flex-row">
-    <Event v-for="ev in currEvents" v-if="currEvents.length > 0" v-bind:key="ev.id" :ev="ev" />
+    <Event v-for="ev in $store.state.currEvents" v-if="$store.state.currEvents.length > 0" v-bind:key="ev.id"
+      :ev="ev" />
     <p v-else>No upcoming events</p>
   </div>
 
   <h3>Past events</h3>
   <div class="d-flex flex-row">
-    <Event v-for="ev in pastEvents" v-if="pastEvents.length > 0" v-bind:key="ev.id" :ev="ev" />
+    <Event v-for="ev in $store.state.pastEvents" v-if="$store.state.pastEvents.length > 0" v-bind:key="ev.id"
+      :ev="ev" />
     <p v-else>No past events</p>
   </div>
 </template>
 
 <script>
-import { __events__ } from '@/constants'
+
 import Event from './Event.vue';
-import Axios from 'axios';
 
 export default {
   name: "Events",
   components: { Event },
-  data() {
-    return {
-      currEvents: [],
-      pastEvents: []
-    }
-  },
-
   methods: {
   },
 
   created() {
-    let AllEvents = []
-    Axios.get(__events__)
-      .then(res => AllEvents = res.data)
-      .then(() => {
-        if (AllEvents.length > 0) {
-          this.currEvents = AllEvents.filter(ev => ev.activeStatus);
-          this.pastEvents = AllEvents.filter(ev => !ev.activeStatus);
-        }
-      })
-      .catch(err => console.log(err.message))
+    this.$store.dispatch("getAllEvents")
   }
 }
 

3679c444c6292ef13a54eebfa5df777782f4f298
Shubham Bajaj <77834591+Antimalware-exe@users.noreply.github.com>
2022-12-17 20:42:37 +0100

    wrapped div in span tag for convention purpose (#12)
    * implemented vue store feature
    
    * changed convention with v-if and v-for

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -1,15 +1,17 @@
 <template>
   <h3>Upcoming events</h3>
   <div class="d-flex flex-row">
-    <Event v-for="ev in $store.state.currEvents" v-if="$store.state.currEvents.length > 0" v-bind:key="ev.id"
-      :ev="ev" />
+    <span v-if="$store.state.currEvents.length > 0">
+      <Event v-for="ev in $store.state.currEvents" v-bind:key="ev.id" :ev="ev" />
+    </span>
     <p v-else>No upcoming events</p>
   </div>
 
   <h3>Past events</h3>
   <div class="d-flex flex-row">
-    <Event v-for="ev in $store.state.pastEvents" v-if="$store.state.pastEvents.length > 0" v-bind:key="ev.id"
-      :ev="ev" />
+    <span v-if="$store.state.pastEvents.length > 0">
+      <Event v-for="ev in $store.state.pastEvents" v-bind:key="ev.id" :ev="ev" />
+    </span>
     <p v-else>No past events</p>
   </div>
 </template>

62499202cb2d0b4a414fd3a2ed790e1c0ed368e5
Shubham Bajaj <77834591+Antimalware-exe@users.noreply.github.com>
2022-12-19 04:04:17 +0100

    Merging working dev code with master (#15)
    * configured inkline
    
    * Added admin login page in bootstrap: removed inkline (#5)
    
    * Added login route
    
    * added axios and integrated with login component
    
    * removed inkline, added bootstrap, added login page
    
    * view events in admin page (#6)
    
    * Added login route
    
    * added axios and integrated with login component
    
    * removed inkline, added bootstrap, added login page
    
    * display events on admin page
    
    * Donate (#8)
    
    * Integrations and installation of Inline
    
    * working inkline
    
    * Navbar and donation form
    
    * transpileDependencies: true
    
    * Donate form
    
    * NavBar routing
    
    * from bajaj to dev (#9)
    
    * Added login route
    
    * added axios and integrated with login component
    
    * removed inkline, added bootstrap, added login page
    
    * display events on admin page
    
    * changed few event style
    
    * Navbar, homepage, Donate page
    
    * Update .gitignore
    
    * donation db
    
    * implemented vue store feature (#10)
    
    * Donate component
    
    * registration form merger in dev
    
    * added main.scss
    
    * registration form
    
    Co-authored-by: Shubham Bajaj <77834591+Antimalware-exe@users.noreply.github.com>
    
    * wrapped div in span tag for convention purpose (#12)
    
    * implemented vue store feature
    
    * changed convention with v-if and v-for
    
    * added admin tables and wired them up with db (#14)
    
    * implemented vue store feature
    
    * changed convention with v-if and v-for
    
    * added admin tables and wired up with db
    
    Co-authored-by: Harsha Poojari <seeker.goblet5der@gmail.com>
    Co-authored-by: Supriya12014 <103660270+Supriya12014@users.noreply.github.com>

--- src/store/index.js
+++ src/store/index.js
@@ -6,12 +6,24 @@ export default createStore({
   state: {
     currEvents: [],
     pastEvents: [],
+    allDonations: [],
+    allApplicants: [],
+    existingVolunteers: [],
   },
   getters: {},
   mutations: {
     setEvents(state, AllEvents) {
       state.currEvents = AllEvents.filter((ev) => ev.activeStatus);
       state.pastEvents = AllEvents.filter((ev) => !ev.activeStatus);
+    },
+    setDonations(state, donations) {
+      state.allDonations = [...donations];
+    },
+    setApplicants(state, applicants) {
+      state.allApplicants = [...applicants];
+    },
+    setVolunteers(state, volunteers) {
+      state.existingVolunteers = [...volunteers];
     },
   },
   actions: {
@@ -22,6 +34,50 @@ export default createStore({
         .then(() => {
           if (AllEvents.length > 0) {
             state.commit("setEvents", AllEvents);
+          }
+        })
+        .catch((err) => console.log(err.message));
+    },
+    getDonations(state) {
+      let allDonations = [];
+      Axios.get(url.__donations__)
+        .then((res) => (allDonations = res.data))
+        .then(() => {
+          if (allDonations.length > 0) {
+            state.commit("setDonations", allDonations);
+          }
+        })
+        .catch((err) => console.log(err.message));
+    },
+    getApplicants(state) {
+      let all = [];
+      Axios.get(url.__applicatiions__)
+        .then((res) => (all = res.data))
+        .then(() => {
+          if (all.length > 0) {
+            state.commit("setApplicants", all);
+          }
+        })
+        .catch((err) => console.log(err.message));
+    },
+    getApplicants(state) {
+      let all = [];
+      Axios.get(url.__applicatiions__)
+        .then((res) => (all = res.data))
+        .then(() => {
+          if (all.length > 0) {
+            state.commit("setApplicants", all);
+          }
+        })
+        .catch((err) => console.log(err.message));
+    },
+    getVolunteers(state) {
+      let all = [];
+      Axios.get(url.__volunteers__)
+        .then((res) => (all = res.data))
+        .then(() => {
+          if (all.length > 0) {
+            state.commit("setVolunteers", all);
           }
         })
         .catch((err) => console.log(err.message));
--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,22 +1,59 @@
 <template>
-  <div class="p-2 d-flex flex-row admin-body">
-    <!-- <div class="sideNav">Nav</div> -->
+  <div class="d-flex flex-row body-container">
     <div class="admin-body">
       <Events />
+      <Table Heading="Donators" :headRow="donationHeader" :contents="$store.state.allDonations"
+        @on-row-click="onDonateRowClick" />
+      <Table Heading="New Applicants" :headRow="applicantHeader" :contents="$store.state.allApplicants"
+        @on-row-click="onApplicationsRowClick" />
+      <Table Heading="Existing Volunteers" :headRow="volunteerHeader" :contents="$store.state.existingVolunteers"
+        @on-row-click="onVolunteersRowClick" />
     </div>
   </div>
 </template>
 <script>
 import Events from '@/components/Events.vue'
+import Table from '@/components/AdminTable.vue'
 
 export default {
   name: "AdminView",
-  components: { Events }
+  components: { Events, Table },
+  data() {
+    return {
+      donationHeader: ["#", "First name", "Last Name", "Email", "Frequency", "Amount (₹)"],
+      applicantHeader: ["#", "First name", "Last Name", "Email", "Education", "Team preference"],
+      volunteerHeader: ["#", "First name", "Last Name", "Email", "Education", "Team"]
+    }
+  },
+  created() {
+    this.$store.dispatch('getDonations')
+    this.$store.dispatch('getApplicants')
+    this.$store.dispatch('getVolunteers')
+  },
+  methods: {
+    onDonateRowClick(item) {
+      console.log(`Donate table record: ${item.amount}`)
+    },
+    onApplicationsRowClick(item) {
+      console.log(`Applicants table record: ${item.team}`)
+    },
+    onVolunteersRowClick(item) {
+      console.log(`Volunteers table record: ${item.address}`)
+    }
+  }
 }
 </script>
 <style scoped>
+.body-container {
+  padding: 1rem 100px;
+  justify-content: flex-start;
+}
+
 .admin-body {
   /* border: 1px solid blue; */
   text-align: start;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 }
 </style>

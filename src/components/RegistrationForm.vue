f0d10df8ecc979835d0c3031ffd761037c43724c
supriya kaur <supriyakaur777@gmail.com>
2022-12-19 21:34:50 +0100

    data binding

--- src/components/RegistrationForm.vue
+++ src/components/RegistrationForm.vue
@@ -1,29 +1,38 @@
 <template>
-  <div class="container pb-2 mb-2">
+  <h1>Volunteer Process</h1>
+  <p1
+    >Decide which causes you care about. Identify the skills and knowledge you
+    can offer. Create a volunteer resume. Determine how often you can volunteer.
+    Research volunteer opportunities in your community. Get all the relevant
+    details for the volunteer position. Apply for the volunteer position. Follow
+    up after you apply. Complete any required training. Begin with a limited
+    commitment.</p1
+  >
+  <h1 class="mt-2 md-3 mb-2 fw-bold">RegisrtrationForm</h1>
+  <div class="container">
     <div class="position-relative top-0 right-0">
-      <h5 class="mt-2 md-3">Personal Details</h5>
+      <h5 class="mt-2 md-3 mb-2 fw-bold">Personal Details</h5>
     </div>
-    <div v-if="page === 1">
+    <div class="form" v-if="page === 1">
       <div class="mb-3">
         <div class="row">
           <div class="col">
-            <label for="firstname" class="form-label">First Name </label>
+            <p>First Name*{{}}</p>
             <input
-              id="firstname"
-              type="text"
+              v-model="firstname"
+              placeholder="firstname"
+              aria-label="first name"
               class="form-control"
-              placeholder="First name"
-              aria-label="First name"
             />
           </div>
           <div class="col">
-            <label for="lastname" class="form-label">Last Name </label>
+            <p>Last Name*{{}}</p>
+
             <input
-              id="lastname"
-              type="text"
-              class="form-control"
-              placeholder="Last name"
+              v-model="lastname"
+              placeholder="lastname"
               aria-label="Last name"
+              class="form-control"
             />
           </div>
         </div>
@@ -31,41 +40,38 @@
       <div class="mb-3">
         <div class="row">
           <div class="col">
-            <label for="dob" class="form-label">D.O.B</label>
+            <p>D.O.B*{{}}</p>
             <input
               id="dob"
               type="date"
+              v-model="dob"
               class="form-control"
               placeholder="D.O.B"
-              aria-label="First name"
+              aria-label="dob"
             />
           </div>
           <div class="col">
-            <label for="gender" class="form-label">Gender</label>
             <div id="gender">
               <span class="me-4"
-                ><input
-                  class="form-check-input"
-                  type="radio"
-                  name="flexRadioDefault"
-                  id="flexRadioDefault1"
-                  checked
-                />
-                <label class="form-check-label ms-1" for="flexRadioDefault1">
-                  Female
-                </label>
+                ><div>Gender* {{}}</div>
+                <div class="form-check form-check-inline">
+                  <input
+                    type="radio"
+                    id="one"
+                    value="One"
+                    v-model="pickedGender"
+                  />
+                  <label for="one">Male</label>
+
+                  <input
+                    type="radio"
+                    id="two"
+                    value="Two"
+                    v-model="pickedGender"
+                  />
+                  <label for="two">Female</label>
+                </div>
               </span>
-              <span>
-                <input
-                  class="form-check-input"
-                  type="radio"
-                  name="flexRadioDefault"
-                  id="flexRadioDefault2"
-                />
-                <label class="form-check-label ms-1" for="flexRadioDefault2">
-                  Male
-                </label></span
-              >
             </div>
           </div>
         </div>
@@ -73,23 +79,21 @@
       <div class="mb-3">
         <div class="row">
           <div class="col">
-            <label for="mobile" class="form-label"> Mobile Number</label>
+            <p>Mobile Number*{{}}</p>
             <input
-              id="mobile"
-              type="text"
-              class="form-control"
+              v-model="mobile"
               placeholder="10 digit number"
-              aria-label="First name"
+              aria-label="mobile"
+              class="form-control"
             />
           </div>
           <div class="col">
-            <label for="email" class="form-label">Email </label>
+            <p>Email*{{}}</p>
             <input
-              id="email"
-              type="text"
+              v-model="email"
+              placeholder="email@gmail.com"
+              aria-label="email"
               class="form-control"
-              placeholder="Email@gmail.com"
-              aria-label="Last name"
             />
           </div>
         </div>
@@ -97,31 +101,32 @@
       <div class="mb-3">
         <div class="row">
           <div class="col">
-            <label for="city" class="form-label">City of Residence </label>
+            <p>City of residence*{{}}</p>
             <input
-              id="city"
-              type="text"
+              v-model="city"
+              placeholder="city"
+              aria-label="city"
               class="form-control"
-              placeholder="City Name"
-              aria-label="First name"
             />
           </div>
           <div class="col">
-            <label for="profession" class="form-label">Profession </label>
+            <p>Education Background*{{}}</p>
             <input
-              id="profession"
-              type="text"
+              v-model="education"
+              placeholder="education"
+              aria-label="education"
               class="form-control"
-              placeholder="Profession"
-              aria-label="Last name"
             />
           </div>
         </div>
       </div>
     </div>
-    <div class="dumbClass" v-else>
+    <div
+      class="dumbClass width: 953px; height: 1020px; left: 245px; top: 632px"
+      v-else
+    >
       <div class="row">
-        <label for="firstname" class="form-label">Language profeciency</label>
+        <label for="firstname" class="form-label">Language profeciency*</label>
       </div>
       <div class="row">
         <div class="col"></div>
@@ -134,6 +139,7 @@
         <div class="col">Marathi</div>
         <div class="col">
           <input
+            v-bind="marathiLanguage"
             class="form-check-input"
             type="radio"
             name="marathiRadio"
@@ -170,6 +176,7 @@
         <div class="col">Hindi</div>
         <div class="col">
           <input
+            v-bind="hindiLanguage"
             class="form-check-input"
             type="radio"
             name="HindiRadio"
@@ -206,6 +213,7 @@
         <div class="col">English</div>
         <div class="col">
           <input
+            v-bind="englishLanguage"
             class="form-check-input"
             type="radio"
             name="Englishradio"
@@ -238,69 +246,84 @@
           />
         </div>
       </div>
-      <div class="row">
-        <div class="col">
-          <div class="dropdown">
-            <button
-              class="btn btn-secondary dropdown-toggle"
-              type="button"
-              id="dropdownMenuButton1"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Team prefrence
-            </button>
-            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-              <li><a class="dropdown-item" href="#">Teaching</a></li>
-              <li><a class="dropdown-item" href="#">Operational</a></li>
-              <li><a class="dropdown-item" href="#">Social media</a></li>
-            </ul>
-          </div>
+      <div class="row mt-5">
+        <div class="col-md-4">
+          <div>Team Preference* {{}}</div>
+
+          <select v-model="selectedTeam">
+            <option disabled value="">Please select one</option>
+            <option>Teaching</option>
+            <option>Operational</option>
+            <option>Social Media</option>
+          </select>
         </div>
-        <div class="col">
-          <div class="dropdown">
-            <button
-              class="btn btn-secondary dropdown-toggle"
-              type="button"
-              id="dropdownMenuButton1"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Mode of Teaching
-            </button>
-            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-              <li><a class="dropdown-item" href="#">Offline</a></li>
-              <li><a class="dropdown-item" href="#">Online</a></li>
-              <li><a class="dropdown-item" href="#">Hybrid</a></li>
-            </ul>
-          </div>
+
+        <div class="col-md-6">
+          <div>Mode of Teaching* {{}}</div>
+
+          <select v-model="selectedMode">
+            <option disabled value="">Please select one</option>
+            <option>Offline</option>
+            <option>Online</option>
+            <option>Hybrid</option>
+          </select>
         </div>
       </div>
-      <div class="row">How you know about us?</div>
-      <div class="col"><div class="form-check">
-  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-  <label class="form-check-label" for="flexRadioDefault1">
-    Social media
-  </label>
-</div>
-<div class="form-check">
-  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
-  <label class="form-check-label" for="flexRadioDefault2">
-    Friends
-  </label>
-</div></div>
-<div class="col"><div class="form-check">
-  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-  <label class="form-check-label" for="flexRadioDefault1">
-    Website
-  </label>
-</div>
-<div class="form-check">
-  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
-  <label class="form-check-label" for="flexRadioDefault2">
-    Others
-  </label>
-</div></div>
+      <div class="row mt-4 md-4">How you know about us?</div>
+      <div class="col-md-3">
+        <div class="form-check">
+          <input
+            v-model="pickedReference"
+            class="form-check-input"
+            type="radio"
+            name="flexRadioDefault"
+            id="flexRadioDefault1"
+          />
+          <label class="form-check-label" for="flexRadioDefault1">
+            Social media
+          </label>
+        </div>
+        <div class="form-check">
+          <input
+            v-model="pickedReference"
+            class="form-check-input"
+            type="radio"
+            name="flexRadioDefault"
+            id="flexRadioDefault2"
+            checked
+          />
+          <label class="form-check-label" for="flexRadioDefault2">
+            Friends
+          </label>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="form-check">
+          <input
+            v-model="pickedReference"
+            class="form-check-input"
+            type="radio"
+            name="flexRadioDefault"
+            id="flexRadioDefault3"
+          />
+          <label class="form-check-label" for="flexRadioDefault3">
+            Website
+          </label>
+        </div>
+        <div class="form-check">
+          <input
+            v-model="pickedReference"
+            class="form-check-input"
+            type="radio"
+            name="flexRadioDefault"
+            id="flexRadioDefault4"
+            checked
+          />
+          <label class="form-check-label" for="flexRadioDefault4">
+            Others
+          </label>
+        </div>
+      </div>
     </div>
 
     <div class="pager">
@@ -312,7 +335,11 @@
       >
         Prev
       </button>
-      <button type="button" class="btn btn-primary" @click="pageUp">
+      <button
+        type="page === 2 ? 'submit' : 'button'"
+        class="btn btn-primary"
+        @click="pageUp"
+      >
         {{ page === 2 ? "Submit" : "Next" }}
       </button>
     </div>
@@ -326,9 +353,24 @@ export default {
   name: "RegisrtrationForm",
   data() {
     return {
+      dob: "",
+      firstname: "",
+      lastname: "",
+      mobile: "",
+      email: "",
+      city: "",
+      education: "",
       page: 1,
+      selectedTeam: "",
+      selectedMode: "",
+      pickedReference: "",
+      pickedGender: "",
+      marathiLanguage: "",
+      hindiLanguage: "",
+      englishLanguage: "",
     };
   },
+
   methods: {
     pageDown() {
       if (this.page > 1) {
@@ -339,6 +381,23 @@ export default {
       if (this.page < 2) {
         this.page++;
       } else if (this.page === 2) {
+        const payload = {
+          firstname: this.firstname,
+          dob: this.dob,
+          lastname: this.lastname,
+          mobile: this.mobile,
+          email: this.email,
+          city: this.city,
+          education: this.education,
+          selectedTeam: this.selectedTeam,
+          selectedMode: this.selectedMode,
+          pickedReference: this.pickedReference,
+          pickedGender: this.pickedGender,
+          marathiLanguage: this.marathiLanguage,
+          hindiLanguage: this.hindiLanguage,
+          englishLanguage: this.englishLanguage,
+        };
+        console.log(payload);
         console.log("Submitted");
       }
     },
@@ -348,29 +407,19 @@ export default {
 
 <style scoped>
 .container {
-  border: 1px solid #d9d9d9;
-  left: 356px;
-  top: 251px;
+  border: 2px solid #d9d9d9;
+
   border-radius: 2px;
+  width: 750px;
+  height: 460px;
+  left: 241px;
+  top: 189px;
 }
 
-.form-text {
-  margin-bottom: 0.5rem;
-  font-size: null;
-  font-style: null;
-  font-weight: null;
+.form {
+  text-align: start;
 }
-.input- {
-  font-family: Open Sans;
-  font-size: 15px;
-  font-weight: 400;
-  line-height: 20px;
-  position: absolute;
-  background: #d9d9d9c2;
-}
-.form-horizontal .control-label {
-  text-align: left;
-}
+
 .mb-3 {
   margin-left: (spacer * 0.25) !important;
 }
@@ -378,7 +427,8 @@ export default {
   display: flex;
   justify-content: space-between;
 }
-.dumbClass {
-  border: 1px solid red;
+
+.form-check-label {
+  text-align-last: left;
 }
 </style>

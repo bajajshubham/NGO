bbfdc7f510abf0759278740c9e274e03d194780f
Shubham Bajaj <77834591+Antimalware-exe@users.noreply.github.com>
2022-12-20 13:11:34 +0100

    Dev (#25)
    * configured inkline
    
    * Added admin login page in bootstrap: removed inkline (#5)
    
    * Added login route
    
    * added axios and integrated with login component
    
    * removed inkline, added bootstrap, added login page
    
    * view events in admin page (#6)
    
    * Added login route
    
    * added axios and integrated with login component
    
    * removed inkline, added bootstrap, added login page
    
    * display events on admin page
    
    * Donate (#8)
    
    * Integrations and installation of Inline
    
    * working inkline
    
    * Navbar and donation form
    
    * transpileDependencies: true
    
    * Donate form
    
    * NavBar routing
    
    * from bajaj to dev (#9)
    
    * Added login route
    
    * added axios and integrated with login component
    
    * removed inkline, added bootstrap, added login page
    
    * display events on admin page
    
    * changed few event style
    
    * Navbar, homepage, Donate page
    
    * Update .gitignore
    
    * donation db
    
    * implemented vue store feature (#10)
    
    * Donate component
    
    * registration form merger in dev
    
    * added main.scss
    
    * registration form
    
    Co-authored-by: Shubham Bajaj <77834591+Antimalware-exe@users.noreply.github.com>
    
    * wrapped div in span tag for convention purpose (#12)
    
    * implemented vue store feature
    
    * changed convention with v-if and v-for
    
    * added admin tables and wired them up with db (#14)
    
    * implemented vue store feature
    
    * changed convention with v-if and v-for
    
    * added admin tables and wired up with db
    
    * refactoring changes (#16)
    
    * implemented vue store feature
    
    * changed convention with v-if and v-for
    
    * added admin tables and wired up with db
    
    * refactored vuex store with util
    
    * from Admin page (#17)
    
    * implemented vue store feature
    
    * changed convention with v-if and v-for
    
    * added admin tables and wired up with db
    
    * refactored vuex store with util
    
    * tweaked navbar, made it dynamic, setup routes
    
    * added details pages
    
    * Supriya (#18)
    
    * added main.scss
    
    * registration form
    
    * data binding
    
    Co-authored-by: supriya kaur <supriyakaur777@gmail.com>
    
    * Donate (#19)
    
    * Footer and Donate form
    
    * Home page
    
    * Contact component and db connection
    
    * Events tab
    
    * Contact us page
    
    * donate
    
    Co-authored-by: Shubham Bajaj <77834591+Antimalware-exe@users.noreply.github.com>
    
    * updated db
    
    * resolved donation frequency bug in donate form
    
    * Rename EventsView.Vue to EventsView.vue
    
    * Donate (#20)
    
    * Footer and Donate form
    
    * Home page
    
    * Contact component and db connection
    
    * Events tab
    
    * Contact us page
    
    * donate
    
    * footer
    
    Co-authored-by: Shubham Bajaj <77834591+Antimalware-exe@users.noreply.github.com>
    
    * Updated db
    
    * Events
    
    * Deleted events
    
    * added events view
    
    Co-authored-by: Harsha Poojari <seeker.goblet5der@gmail.com>
    Co-authored-by: Supriya12014 <103660270+Supriya12014@users.noreply.github.com>
    Co-authored-by: supriya kaur <supriyakaur777@gmail.com>

--- src/views/EventsView.vue
+++ src/views/EventsView.vue
@@ -1,25 +1,25 @@
 <template>
-  <div class="display-events">
-    <Events />
-  </div>
-</template>
-
-<script>
-import Events from "../components/Events.vue";
-export default {
-  name: "ViewEvents",
-  components: {
-    Events,
-  },
-};
-</script>
-<style scoped>
-.display-events{
-    margin-top: 100px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-family: 'Work Sans';
-}
-</style>
+    <div class="display-events">
+      <Events />
+    </div>
+  </template>
+  
+  <script>
+  import Events from "../components/Events.vue";
+  export default {
+    name: "ViewEvents",
+    components: {
+      Events,
+    },
+  };
+  </script>
+  <style scoped>
+  .display-events{
+      margin-top: 100px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-family: 'Work Sans';
+  }
+  </style>
